212e69de44d23135a9887edf7aaaaddb4dafa79f
uije <zettafantasy@gmail.com>
2016-02-10 17:24:24 +0900

    임시파일 삭제 및 주소 및 지도 업데이트

--- js/commonDicBuilder.js
+++ js/commonDicBuilder.js
@@ -3,8 +3,8 @@ var commonDicBuilder = commonDicBuilder || {};
 commonDicBuilder.menu = {};
 commonDicBuilder.menu.data = [
     {name:"D.I.C 투명교정", link:"./about_dic.html"},
-    {name:"D.I.C 제품소개", link:"./product.html"},
-    {name:"주문공간", link:"./order.html"},
+    //{name:"D.I.C 제품소개", link:"./product.html"},
+    //{name:"주문공간", link:"./order.html"},
     {name:"고객센터", link:"./customer.html"},
     {name:"회사소개", link:"./introduce.html"}
 ];
@@ -23,9 +23,9 @@ commonDicBuilder.menu.build = function(){
 commonDicBuilder.footer = {};
 commonDicBuilder.footer.build = function(){
     var footerHTML = '<p> ㈜ 디아이씨</p>' + 
-        '<address>   대표이사 : 박재형 <br />   사업자등록번호 : 171-97-00016 오산시 원동 494-2번지 3F 전관' +
-        '<br />TEL : 031 902 8500' +
-        '<br />FAX : 031 372 3047' +
+        '<address>   대표이사 : 박재형 <br />   사업자등록번호 : 171-97-00016 경기도 고양시 일산동구 고봉로 32-19 남정씨티프라자 705호' +
+        '<br />TEL : 031 915 6600' +
+        '<br />FAX : 031 915 9600' +
         '<br />E-mail : dic00016@naver.com' + 
         '<br />COPYRIGHT 2016 ALL RIGHTS RESERVED' +
         '</address>';
